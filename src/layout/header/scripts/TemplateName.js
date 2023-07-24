26cad784fd7d2213899271bb2b40eb8167f32348
Vadim <karandashevva@gmail.com>
2023-07-24 09:32:54 +0800

    base_version

---
+++ src/layout/header/scripts/TemplateName.js
@@ -0,0 +1,6 @@
+/*
+* JavaScript
+* Name:
+* Path:
+ */
+console.log('Файл: [...] подключен!');
---
+++ src/scripts.js
@@ -0,0 +1,11 @@
+// --- polyfill
+
+// --- libs
+
+// ---modules
+
+// --- elements
+
+// --- section
+
+//---
